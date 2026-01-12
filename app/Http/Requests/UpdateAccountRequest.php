44f47f75ce9a507563bec33b7fc0b13b4963d1bf
RakaMuttaqin <rakamuttaqin13@gmail.com>
2026-01-13 00:02:25 +0700

    Store & Update Account Request

--- app/Http/Requests/UpdateAccountRequest.php
+++ app/Http/Requests/UpdateAccountRequest.php
@@ -11,7 +11,7 @@ class UpdateAccountRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class UpdateAccountRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string',
+            'balance' => 'required|numeric',
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,8 +9,20 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
 
-Route::middleware('auth:api')->group(function(){
+Route::middleware('auth:api')->group(function () {
+
+    // auth
     Route::get('/me', [AuthController::class,'me']);
     Route::post('/logout', [AuthController::class,'logout']);
+
+    // user & admin
+    Route::apiResource('accounts', AccountController::class);
+
+    // admin only
+    Route::middleware('role:admin')->prefix('admin')->group(function () {
+        // Route::get('/users', [AdminUserController::class,'index']);
+        // Route::get('/accounts', [AdminAccountController::class,'index']);
+    });
+
 });
 
